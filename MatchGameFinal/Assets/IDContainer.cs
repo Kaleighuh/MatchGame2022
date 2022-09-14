d0706c2f1fee21513c6b96e8827dc8fe33e96452
Kaleah <10958501@uvu.edu>
2022-09-14 14:32:56 -0600

    Halfway through Match Game Lectures

---
+++ MatchGameFinal/Assets/IDContainer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class IDContainer : MonoBehaviour
+{
+   public IDObj IDs;
+   
+}
--- MatchGameFinal/Assets/MatchBehaviour.cs
+++ MatchGameFinal/Assets/MatchBehaviour.cs
@@ -5,6 +5,21 @@ public class MatchBehaviour : MonoBehaviour
     public IDObj IDs;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(IDs);
+        //Debug.Log(IDs);
+        //Debug.Log(other.GetComponent<IDContainer>().IDs);
+        var tempObj= other.GetComponent<IDContainer>();
+        if (tempObj != null)
+            return;
+        var otherID = tempObj.IDs;
+        if (otherID == IDs)
+        {
+            Debug.Log("Matched");
+        }
+        else
+        {
+            {
+                Debug.Log("No Match");
+            }
+        }
     }
 }
